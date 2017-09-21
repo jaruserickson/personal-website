6f51728f330b1d56c81d19e7d22504d65a1bf468
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2017-09-21 18:45:10 -0400

    update stuffs

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
                 <Header 
                   name={'jacob erickson'}
                   title={'software engineer'}
-                  videoUri={'https://s3.amazonaws.com/jaruserickson/new_broll.mp4'}
+                  videoUri={'https://s3.amazonaws.com/jaruserickson/newer_broll.mp4'}
                   social={{
                     github: 'jaruserickson',
                     instagram: 'jaruserickson',
-                    resume: 'path-to-resume',
+                    resume: 'resume.pdf',
                     linkedin: 'jaruserickson',
                     lastfm: 'jarus_e'
                   }}
