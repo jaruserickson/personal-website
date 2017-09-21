624ecc25e78cb4c0b163f2573bc4961944972ae7
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2017-09-21 18:13:58 -0400

    update without bottmom and errors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Section from './components/Section'
-import Title from './components/Title'
+
 class App extends Component {
     render() {
         return (
@@ -11,7 +10,7 @@ class App extends Component {
                 <Header 
                   name={'jacob erickson'}
                   title={'software engineer'}
-                  videoUri={'https://s3.amazonaws.com/jaruserickson/broll.mp4'}
+                  videoUri={'https://s3.amazonaws.com/jaruserickson/new_broll.mp4'}
                   social={{
                     github: 'jaruserickson',
                     instagram: 'jaruserickson',
@@ -20,37 +19,6 @@ class App extends Component {
                     lastfm: 'jarus_e'
                   }}
                 />
-                <div className={'subcontainer'}>
-                    <Title>education</Title>
-                    <Section
-                        title={'University of Toronto'}
-                        subtitle={'Computer Science'}
-                        time={'September 2015 - May 2020'}
-                        content={'focuses in Artificial Intelligence and Computer Vision'}
-                    />
-
-                    <Title>work experience</Title>
-                    <Section
-                        title={'KOMBINE AI'}
-                        subtitle={'Junior Full Stack Developer'}
-                        time={'May 2017 - August 2017'}
-                        content={'content'}
-                    />
-
-                    <Title>extracurriculars</Title>
-                    <Section
-                        title={'uoft counter-strike club'}
-                        subtitle={'lead web dev'}
-                        time={'January 2017 - current'}
-                        content={'content'}
-                    />
-                    <Section
-                        title={'uoft esports club'}
-                        subtitle={'lead web dev'}
-                        time={'January 2017 - current'}
-                        content={'content'}
-                    />
-                </div>
             </div>
         )
     }
--- src/components/Title.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './css/Title.css'
-
-export default ({children}) => (
-    <div>
-        <p className={'grouptitle'}>{children}</p>
-    </div>
-)
