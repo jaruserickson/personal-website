874799cd838e15fa9d2083de40082e8af6b93a3b
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2017-09-28 00:38:48 -0400

    changed videojs to gif

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react'
-import Video from './Video'
+//import Video from './Video'
 import Social from './Social'
 
 import './css/Header.css'
@@ -29,47 +29,13 @@ export default class Header extends Component {
         const {
             name,
             title,
-            videoUri,
+            //videoUri,
             social
         } = this.props
 
-        const {
-            width,
-            height
-        } = this.state
-
-        let thisWidth = 1280
-        let thisHeight = 720
-        if (!( width <= 1280 && height <= 720)){
-            if (width > 1280 && height < 1280) {
-                thisWidth = width
-                thisHeight = Math.ceil((8 * width) / 9)
-            } else {
-                thisWidth = Math.ceil((16 * height) / 9)
-                thisHeight = height
-            }
-        }
-
-        const videoOptions = {
-            autoplay: true,
-            controls: false,
-            loop: true,
-            muted: true,
-            fluid: true,
-            loadingSpinner: false,
-            preload: 'auto',
-            sources: [{
-                src: videoUri,
-                type: 'video/mp4'
-            }]
-        }
-
         return (
             <div className={'container'}>
-                <div style={{width: thisWidth, height: thisHeight, position: 'absolute', top: 0}}>
-                    <Video { ...videoOptions }/>
-                </div>
-                <div style={{position: 'absolute', zIndex: 1, top: 0, width: thisWidth, height: thisHeight}}/>
+                <div className={'page-bg'}/>
                 <div className={'textcontainer'}>
                     <p className={'name'}>{name}</p>
                     <p className={'title'}>{title}</p>
@@ -80,3 +46,40 @@ export default class Header extends Component {
     }
     
 }
+
+//all this code is for the old videojs background
+        // const {
+        //     width,
+        //     height
+        // } = this.state
+
+        // let thisWidth = 1280
+        // let thisHeight = 720
+        // if (!( width <= 1280 && height <= 720)){
+        //     if (width > 1280 && height < 1280) {
+        //         thisWidth = width
+        //         thisHeight = Math.ceil((8 * width) / 9)
+        //     } else {
+        //         thisWidth = Math.ceil((16 * height) / 9)
+        //         thisHeight = height
+        //     }
+        // }
+
+        // const videoOptions = {
+        //     autoplay: true,
+        //     controls: false,
+        //     loop: true,
+        //     muted: true,
+        //     fluid: true,
+        //     loadingSpinner: false,
+        //     preload: 'auto',
+        //     sources: [{
+        //         src: videoUri,
+        //         type: 'video/mp4'
+        //     }]
+        // }
+        /*
+        <div style={{width: thisWidth, height: thisHeight, position: 'absolute', top: 0}}>
+            <Video { ...videoOptions }/>
+        </div>
+        */
